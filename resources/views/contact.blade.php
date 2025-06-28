ac68f5c5d2f7d734a007cb2b33bc8ec7a48cee27
Hammad <hmdshfq@outlook.com>
2025-06-28 23:02:44 +0200

    add tailwind css

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,4 @@
 <x-layout>
+    <x-slot:heading>Contact Page</x-slot:heading>
     <h1>Hello from the contact on Laravel</h1>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,4 @@
 <x-layout>
+    <x-slot:heading>Home Page</x-slot:heading>
     <h1>Hello from the homepage on Laravel</h1>
 </x-layout>
