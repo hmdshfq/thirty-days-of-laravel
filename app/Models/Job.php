dc6f8f285a3e7ff72a005ab7ff606bb9870b2f68
Hammad <hmdshfq@outlook.com>
2025-07-03 20:38:07 +0200

    Refactor Job model to use integer IDs and simplify job retrieval in routes

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Models;
+use Illuminate\Support\Arr;
 
 class Job
 {
@@ -7,20 +9,24 @@ class Job
     {
         return [
             [
-                "id" => "1",
+                "id" => 1,
                 "title" => "Director",
                 "salary" => '$50,000',
             ],
             [
-                "id" => "2",
+                "id" => 2,
                 "title" => "Programmer",
                 "salary" => '$10,000',
             ],
             [
-                "id" => "3",
+                "id" => 3,
                 "title" => "Teacher",
                 "salary" => '$40,000',
             ],
         ];
+    }
+
+    public static function find(int $id): array {
+        return Arr::first(static::all(), fn($job) => $job["id"] === $id);
     }
 }
